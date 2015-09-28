6431b1e18b7890cab149e5526764e84aa11045ad
al223jw <al223jw@student.lnu.se>
2015-09-28 15:54:58 +0200

    First

--- Workshop2/controller/placeholder.cs
+++ Workshop2/controller/placeholder.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Workshop2.view;
+using Workshop2.model;
 
 namespace Workshop2.controller
 {
     class placeholder
     {
+        Menuview menuview = new Menuview();
+        Member member = new Member();
     }
 }
