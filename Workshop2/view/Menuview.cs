5edd2f7742ac99c409a14b1d60f72aa47bbee384
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-09-29 13:06:04 +0200

    Adding some more code to View
    More view

--- Workshop2/view/Menuview.cs
+++ Workshop2/view/Menuview.cs
@@ -13,9 +13,11 @@ namespace Workshop2.view
         string firstname = "";
         string lastname = "";
         int persnr;
+        string userinput;
+        string finalcheck;
         public void Runapplication()
         {
-            placeholder menucontroler = new placeholder();
+            placeholder menucontroller = new placeholder();
             StartMenu();
         }
         public void StartMenu()
@@ -23,10 +25,8 @@ namespace Workshop2.view
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("===================================");
             Console.WriteLine("===            MENU             ===");
-            Console.WriteLine("===================================");
+            Console.WriteLine("===================================\n\n");
             Console.ResetColor();
-            Console.WriteLine("");
-            Console.WriteLine("");
             Console.WriteLine("1.Register person");
             Console.WriteLine("2.Update Person");
             Console.WriteLine("3.Edit person");
@@ -80,7 +80,6 @@ namespace Workshop2.view
 
         public void registerperson()
         {
-            //throw new NotImplementedException();
             Console.WriteLine("Du valde ny person!\n");
             Console.WriteLine("förnamn:?");
             firstname = Console.ReadLine();
@@ -93,31 +92,80 @@ namespace Workshop2.view
 
         }
 
+        //Funktionerna som anropas efter att man har registerat en användare
+        public string getfirstname
+        {
+            get { return firstname; }
+        }
+
+        public string getlastname
+        {
+            get { return lastname; }
+        }
+
+        public int getpersnr
+        {
+            get { return persnr; }
+        }
+
         public void updateperson()
         {
-            //throw new NotImplementedException();
-            Console.WriteLine("Du valde update person!");
+            do
+            {
+                Console.WriteLine("Du valde update person!\n");
+                Console.WriteLine("Välj en person från listan genom att skriva ID:t :\n");
+                //TODO: lista med personer ska hämtas från controllern..
+
+                List<string> Persons = menucontroller.getPersons();
+
+                foreach(var person in Persons)
+                {
+                    Console.Writeline("Firstname: {0}, Lastname: {1}, Persnr: {2}, ID: {3}", person.Firstname,person.Lastname,person.Persnr,person.id);
+                    
+                }
+
+                userinput = Console.ReadLine();
+
+                //Kolla om userinput machar ett id till en person
+
+                if()
+                {
+                    //Görs här
+                }
+
+                if ()
+                {
+                    Console.WriteLine("vill du redigera person:" + person.Firstname + "," + person.Lastname + "," + person.Persnr + "," + person.id +"?");
+                    Console.WriteLine("ja/nej");
+                    finalcheck = Console.ReadLine();
+                    if (finalcheck == "ja")
+                    {
+                        //Redigera person
+                        Console.WriteLine("Temp");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Registeringen avbröts eller så angavs ett ickegjiltigt kommando, återgår till update person.");
+                    }
+                }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
+        
 
         public void editperson()
         {
-            //throw new NotImplementedException();
             Console.WriteLine("Du valde redigera person!");
         }
 
         public void listpersonverbose()
         {
-            //throw new NotImplementedException();
-            Console.WriteLine("Du valde Verbose list!");
+           Console.WriteLine("Du valde Verbose list!");
         }
 
         public void listpersoncompact()
         {
-            //throw new NotImplementedException();
             Console.WriteLine("Du valde compact list!");
         }
-
-
-
     }
 }
